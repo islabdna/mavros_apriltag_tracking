a22d389aad35affe46cfcf9653a8f92d4570d2ca
labuser <islab.dna@gmail.com>
2021-02-24 13:45:42 +0300

    modifing the tracking tf

--- scripts/apriltag_setpoint_publisher.py
+++ scripts/apriltag_setpoint_publisher.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python
 """
-BSD 3-Clause License
-
-Copyright (c) 2020, Mohamed Abdelkader Zahana
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2020, Mohamed Abdelkader.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  """
 import rospy
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, PoseStamped
 from std_msgs.msg import Bool
 import tf
 
@@ -39,8 +34,8 @@ class SetpointPublisher:
     def __init__(self):
 
         self.drone_frame_id_ = rospy.get_param('~drone_frame_id', '/fcu')
-        self.tag_frame_id_ = rospy.get_param('~tag_frame_id', '/pipe_link_lidar')
-        self.tags_topic_ = rospy.get_param('~tags_topic', 'lidar_pipe_detected')
+        self.tag_frame_id_ = rospy.get_param('~tag_frame_id', '/kf_pipe_link')
+        self.tags_topic_ = rospy.get_param('~tags_topic', '/cylinder_centroid_pose')
         self.setpoint_topic_ = rospy.get_param('~setpoint_topic', 'setpoint/relative_pos')
 
         self.tf_listener_ = tf.TransformListener()
@@ -55,20 +50,18 @@ class SetpointPublisher:
         # Relative setpoint publisher
         self.setpoint_pub_ = rospy.Publisher(self.setpoint_topic_, Point, queue_size=10)
 
-        rospy.Subscriber(self.tags_topic_, Bool, self.tagsCallback)
+        rospy.Subscriber(self.tags_topic_, PoseStamped, self.tagsCallback)
 
     # tags callback
     def tagsCallback(self, msg):
         valid = False
         trans = []
-        if msg.data: # make sure detection is valid
-
-            try:
-                (trans,_) = self.tf_listener_.lookupTransform(self.drone_frame_id_, self.tag_frame_id_, rospy.Time(0))
-                valid = True
-            except :
-                rospy.logwarn("No valid TF for the required ")
-                valid = False
+        try:
+            (trans,_) = self.tf_listener_.lookupTransform(self.drone_frame_id_, self.tag_frame_id_, rospy.Time(0))
+            valid = True
+        except :
+            rospy.logwarn("No valid TF for the required ")
+            valid = False
                 
         if valid:
             # for debug
@@ -89,7 +82,7 @@ class SetpointPublisher:
             sp_msg = Point()
             sp_msg.x = 0
             sp_msg.y = 0
-            sp_msg.z = self.alt_from_tag_
+            sp_msg.z = 0
             self.setpoint_pub_.publish(sp_msg)
 
 
